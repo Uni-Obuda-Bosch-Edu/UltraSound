41510e9b3dcc89cf8c6a280128f085aea0ef6327
Szloboda Dániel <sz.danix6@gmail.com>
2015-10-20 13:51:10 +0200

    Sensor Coord init

--- src/UltraSound_System/UltraSound_Impl.java
+++ src/UltraSound_System/UltraSound_Impl.java
@@ -5,17 +5,51 @@ import java.util.Map;
 
 import FrameWork.virtualDataBus.Container;
 
-public class UltraSound_Impl implements UltraSound_Out, UltraSound_IN{
+public class UltraSound_Impl implements UltraSound_Out, UltraSound_IN {
+
+	private int XUltraSoundDistance;
+	private int YUltraSoundDistance;
+
+	private Point LeftUpSensor;
+	private Point RightUpSensor;
+	private Point LeftDownSensor;
+	private Point RightDownSensor;
 
 	Container container;
-	
-	public UltraSound_Impl(){
+
+	public UltraSound_Impl() {
 		init();
 	}
-	private void init(){
+
+	private void init() {
 		container = Container.getInstance();
+		XUltraSoundDistance = 50; // 100*240
+		YUltraSoundDistance = 120;
+
+		LeftUpSensor = new Point();
+		RightUpSensor = new Point();
+		LeftDownSensor = new Point();
+		RightDownSensor = new Point();
+		sensorCalc();
 	}
-	 
+
+	private void sensorCalc() {
+		int carPosX = GetCarPositionXY().x;
+		int carPosY = GetCarPositionXY().y;
+
+		LeftUpSensor.x = carPosX - XUltraSoundDistance; // x-z
+		LeftUpSensor.y = carPosY + YUltraSoundDistance; // y+k
+
+		RightUpSensor.x = carPosX + XUltraSoundDistance; // x+z
+		RightUpSensor.y = carPosY + YUltraSoundDistance; // y+k
+
+		LeftDownSensor.x = carPosX - XUltraSoundDistance; // x-z
+		LeftDownSensor.y = carPosY - YUltraSoundDistance; // y-k
+
+		RightDownSensor.x = carPosX - XUltraSoundDistance; // x+z
+		RightDownSensor.y = carPosY - YUltraSoundDistance; // y-k
+	}
+
 	@Override
 	public Map<Integer, Point> Obstackles() {
 		// TODO Auto-generated method stub
